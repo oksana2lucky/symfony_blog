af00276820a7639a63d4791128e1613b9415039e
Oksana Pampushchenko <oksana2lucky@gmail.com>
2020-10-28 11:31:53 +0200

    Created CRUD Admin controllers

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Entity\Post;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -21,17 +23,16 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Symfony Blog');
+            ->setTitle('Blog Admin');
     }
 
     public function configureMenuItems(): iterable
     {
         return [
             MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
-            MenuItem::section('Blog'),
-           /* MenuItem::linkToCrud('Posts', 'fa fa-file-text', Post::class),
+            MenuItem::section('Manage'),
             MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
-            MenuItem::linkToCrud('Comments', 'fa fa-comment', Comment::class),*/
+            MenuItem::linkToCrud('Posts', 'fa fa-file-text', Post::class),
         ];
     }
 }
