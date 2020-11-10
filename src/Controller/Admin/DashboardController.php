65f58ec837c7ef12d51afcca0b714318fd466639
Oksana Pampushchenko <oksana2lucky@gmail.com>
2020-11-10 14:01:22 +0200

    Implemented user access and role permissions

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -56,12 +56,12 @@ class DashboardController extends AbstractDashboardController
                     ->setAction('new'),
             ]);
 
-        yield MenuItem::section('Settings');
+        yield MenuItem::section('Settings')->setPermission('ROLE_ADMIN');
         yield MenuItem::subMenu('Access')->setSubItems([
                 MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
                 MenuItem::linkToCrud('Add User', 'fa fa-tags', User::class)
                     ->setAction('new'),
-            ]);
+            ])->setPermission('ROLE_ADMIN');
     }
 
     public function configureCrud(): Crud
@@ -72,27 +72,11 @@ class DashboardController extends AbstractDashboardController
 
     public function configureUserMenu(UserInterface $user): UserMenu
     {
-        // Usually it's better to call the parent method because that gives you a
-        // user menu with some menu items already created ("sign out", "exit impersonation", etc.)
-        // if you prefer to create the user menu from scratch, use: return UserMenu::new()->...
         return parent::configureUserMenu($user)
-            // use the given $user object to get the user name
             ->setName($user->getFullName())
-            // use this method if you don't want to display the name of the user
-            ->displayUserName(false)
-
-            // you can return an URL with the avatar image
-            ->setAvatarUrl('https://...')
-            ->setAvatarUrl($user->getProfileImageUrl())
-            // use this method if you don't want to display the user image
-            ->displayUserAvatar(false)
-            // you can also pass an email address to use gravatar's service
-            ->setGravatarEmail($user->getMainEmailAddress())
-
-            // you can use any type of menu item, except submenus
+            ->displayUserName(true)
             ->addMenuItems([
-                MenuItem::linkToRoute('My Profile', 'fa fa-id-card', '...', ['...' => '...']),
-                MenuItem::linkToRoute('Settings', 'fa fa-user-cog', '...', ['...' => '...']),
+               // MenuItem::linkToRoute('My Profile', 'fa fa-id-card', '...', ['...' => '...']),
                 MenuItem::section(),
                 MenuItem::linkToLogout('Logout', 'fa fa-sign-out'),
             ]);
