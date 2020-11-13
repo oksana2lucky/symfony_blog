4cf2a19ced9dc1432d1149b886e1eb64941dcd71
Oksana Pampushchenko <oksana2lucky@gmail.com>
2020-11-13 20:52:43 +0200

    Implemented pagination for posts and comments lists

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,13 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 use App\Entity\Post;
 use App\Entity\Comment;
-use App\Form\CommentFormType;
 
 class PostController extends AbstractController
 {
-    public const LIMIT_POSTS = 8;
+    public const LIMIT_POSTS_BLOCK = 5;
+    public const POSTS_PER_PAGE = 8;
+    public const COMMENTS_PER_PAGE = 8;
 
     /**
      * @Route("/", name="homepage")
@@ -25,10 +27,10 @@ class PostController extends AbstractController
             ->findBy(
                 [],
                 ['created_at' => 'DESC'],
-                self::LIMIT_POSTS
+                self::LIMIT_POSTS_BLOCK
             );
 
-        $popularPosts = $postRepository->findByMoreCommentsCount(self::LIMIT_POSTS);
+        $popularPosts = $postRepository->findByMoreCommentsCount(self::LIMIT_POSTS_BLOCK);
 
         return $this->render('post/index.html.twig', [
             'newest_posts' => $newestPosts,
@@ -38,30 +40,48 @@ class PostController extends AbstractController
 
     /**
      * @Route("/post/all", name="post_all")
+     * @param Request $request
+     * @param PaginatorInterface $paginator
+     * @return Response
      */
-    public function showAll(): Response
+    public function showAll(Request $request, PaginatorInterface $paginator): Response
     {
-        $posts = $this->getDoctrine()->getRepository(Post::class)->findBy(
+        $allPosts = $this->getDoctrine()->getRepository(Post::class)->findBy(
             [],
             ['created_at' => 'DESC']
         );
 
+        $posts = $paginator->paginate(
+            $allPosts,
+            $request->query->getInt('page', 1),
+            self::POSTS_PER_PAGE
+        );
+
         return $this->render('post/all.html.twig', [
-            'posts' => $posts,
+            'posts' => $posts
         ]);
     }
 
     /**
      * @Route("/post/{id}", name="post_view")
      * @param $id
+     * @param Request $request
+     * @param PaginatorInterface $paginator
      * @return Response
      */
-    public function view($id): Response
+    public function view($id, Request $request, PaginatorInterface $paginator): Response
     {
         $post = $this->getDoctrine()->getRepository(Post::class)->find($id);
 
+        $comments = $paginator->paginate(
+            $post->getComments(),
+            $request->query->getInt('page', 1),
+            self::COMMENTS_PER_PAGE
+        );
+
         return $this->render('post/view.html.twig', [
             'post' => $post,
+            'comments' => $comments
         ]);
     }
 }
